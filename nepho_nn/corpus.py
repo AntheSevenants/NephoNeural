86b61c43aa6dd5f9352c65f5c603a2a236140624
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-05 09:30:57 +0200

    Add Corpus class to hold sentences

---
+++ nepho_nn/corpus.py
@@ -0,0 +1,15 @@
+class Corpus:
+	def __init__(self, lemma):
+		self.lemma = lemma
+		self.sentences = {}
+
+	# Sentence: the sentence where the token occurs (list!)
+	# Token index: the zero-based index of that token
+	# Token id: a unique identifier to give to the token
+	def add_sentence(self, sentence, token_index, token_id):
+		sentence_entry = { "sentence": sentence,
+						   "token_index": token_index,
+						   "token_id": token_id }
+
+	def get_sentence(self, sentence_id):
+		return self.sentences[sentence_id]
