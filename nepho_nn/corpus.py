7933f020262282420e200fd35d8fb3635b7a68fb
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-05 09:50:26 +0200

    Add 'file' property to Corpus entries

--- nepho_nn/corpus.py
+++ nepho_nn/corpus.py
@@ -6,10 +6,11 @@ class Corpus:
 	# Sentence: the sentence where the token occurs (list!)
 	# Token index: the zero-based index of that token
 	# Token id: a unique identifier to give to the token
-	def add_sentence(self, sentence, token_index, token_id):
+	def add_sentence(self, sentence, token_index, token_id, file=None):
 		sentence_entry = { "sentence": sentence,
 						   "token_index": token_index,
-						   "token_id": token_id }
+						   "token_id": token_id,
+						   "file": file }
 
 	def get_sentence(self, sentence_id):
 		return self.sentences[sentence_id]
