1e7033cd8e347ed269e45365b331ee5891c7661b
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-07 20:02:14 +0200

    Save context words per sentence

--- nepho_nn/context_words.py
+++ nepho_nn/context_words.py
@@ -1,9 +1,16 @@
 class ContextWords:
 	def __init__(self):
 		self.words = []
+		self.words_in_sentence = {}
+
 		self.token_embeddings = []
 
-	def add(self, word, token_embedding):
+	def add(self, word, token_embedding, sentence_id):
+		if sentence_id not in self.words_in_sentence:
+			self.words_in_sentence[sentence_id] = []
+
+		self.words_in_sentence[sentence_id].append(word)
+
 		if word not in self.words:
 			self.words.append(word)
 			self.token_embeddings.append(token_embedding)
