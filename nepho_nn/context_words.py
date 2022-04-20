9795d66be4426dd096a3c13708968a076b301e0a
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-20 17:54:37 +0200

    Fix context words lemma implementation

--- nepho_nn/context_words.py
+++ nepho_nn/context_words.py
@@ -5,11 +5,14 @@ class ContextWords:
 
 		self.token_embeddings = []
 
+	def add_word(self, sentence_id, word):
+		self.words_in_sentence[sentence_id].append(word)
+
 	def add(self, word, token_embedding, sentence_id):
 		if sentence_id not in self.words_in_sentence:
 			self.words_in_sentence[sentence_id] = []
 
-		self.words_in_sentence[sentence_id].append(word)
+		self.add_word(sentence_id, word)
 
 		if word not in self.words:
 			self.words.append(word)
--- nepho_nn/context_words_lemma.py
+++ nepho_nn/context_words_lemma.py
@@ -1,12 +1,12 @@
 from .context_words import ContextWords
 
 class ContextWordsLemma(ContextWords):
-	def __init__(self):
-		super()
-
 	def add(self, word, sentence_id):
 		# We do this first, because it'll allow us to have unique collections
 		if sentence_id not in self.words_in_sentence:
 			self.words_in_sentence[sentence_id] = set()
 
-		super().add(self, word, None, sentence_id)
+		super().add(word, None, sentence_id)
+
+	def add_word(self, sentence_id, word):
+		self.words_in_sentence[sentence_id].add(word)
